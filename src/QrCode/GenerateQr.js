dbbeec9e70345add421156a1bff42b8b8b83f148
I461268 <d.picus@student.fontys.nl>
2021-05-17 08:39:40 +0200

    generate qr

--- src/QrCode/GenerateQr.js
+++ src/QrCode/GenerateQr.js
@@ -1,29 +1,87 @@
 import React, { useState } from 'react';
 
 export function GenerateQr(){
-    const [qr, getqr] = useState("https://picsum.photos/200");
-    //const [tableNr, setTableNr] = useState("");
-    const [nr, oneup] = useState(0);
+    const [qr, setQr] = useState("https://picsum.photos/200");
+    const [tableNr, setTableNr] = useState();
+    const [cusName, setCusName] = useState("");
+    const [sessionId, setSessionId] = useState("");
+
+    const onCusNameUpdate = (e) => {
+        setCusName(e.target.value);
+    }
+    const onTableNrUpdate = (e) => {
+        setTableNr(parseInt(e.target.value));
+    }
+
+     
+    const CreateSession = (e) =>{
+        e.preventDefault();
+
+        const isTableNrProvided = tableNr && tableNr !== "";
+        const iSCusNameProvided = cusName && cusName !== "";
+
+        if(isTableNrProvided && iSCusNameProvided){
+            const newSession = {
+                tableNumber: tableNr, 
+                name: cusName
+            }
+    
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newSession)
+            };
+    
+            fetch('https://localhost:6001/Session', requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    setSessionId(data.id)
+                    setQr("https://localhost:6001/Session/GetQR/" + data.id);
+                })
+        } else {
+            alert('Please insert a table nr and name');
+        }   
+    }    
    
     return(
         <div>
-            <button
-                    onClick={() => getQrCode()}
-                >
-                Create Qr code
-            </button>
             <div>
-                <img src={qr} alt="qr"/>
-                <h1>{nr}</h1>
+                <form>
+                    <center>
+                    <label>Name</label>
+                    <br/>
+                    <input
+                        value = {cusName}
+                        onChange = {onCusNameUpdate}
+                    />
+                    <br/>
+                    <label>Table Nr</label>
+                    <br/>
+                    <input   
+                        value = {tableNr}
+                        onChange = {onTableNrUpdate}
+                    />
+                    <br/>
+                    <button onClick={CreateSession} >Create Customer QR</button>
+                    </center>
+                </form>
             </div>
+
+            <br/>
+            <div>
+                {/* <button
+                        onClick={() => getQrCode()}
+                    >
+                    Create Qr code
+                </button> */}
+                <div>
+                    <img src={qr} alt="qr"/>
+                    <h1>{sessionId}</h1>
+                </div>
+            </div>
+
         </div>
     );
-    
-    async function getQrCode(){
-        //const response = await fetch("https://localhost:6001/QrCode");
-        oneup(nr + 1)
-        getqr("https://localhost:6001/QrCode/" + nr);
-    }
 }
 
 
